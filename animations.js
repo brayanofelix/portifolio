35870eb1f32140b1921695a36f9aacd195d91f2f
brayanofelix <brayanofelix@gmail.com>
2024-09-20 16:25:19 -0300

    ajustando responsividade

--- animations.js
+++ animations.js
@@ -1,3 +1,5 @@
+let animationPlayed = false; // Flag para verificar se a animação já foi executada
+
 document.addEventListener('scroll', function () {
     const sections = document.querySelectorAll('.section');
 
@@ -8,31 +10,34 @@ document.addEventListener('scroll', function () {
         const screenPosition = window.innerHeight / 1.3;
 
         if (sectionPosition < screenPosition && sectionBottom > 0) {
-            h1.classList.add('animate__animated', 'animate__bounceInDown');
-            h1.style.opacity = 1;
+            // Animação para as seções (excluindo section-1)
+            if (section.classList.contains('section-1') && !animationPlayed) {
+                h1.classList.add('animate__animated', 'animate__bounceInDown');
+                h1.style.opacity = 1;
+                animationPlayed = true; // Marca que a animação já foi executada
+            } else if (!section.classList.contains('section-1')) {
+                h1.classList.add('animate__animated', 'animate__bounceInDown');
+                h1.style.opacity = 1;
+            }
         } else {
             h1.classList.remove('animate__animated', 'animate__bounceInDown');
-            h1.style.opacity = 0;
+            h1.style.opacity = 1;
         }
     });
 });
 
+// Este código é executado após o DOM estar completamente carregado
 document.addEventListener('DOMContentLoaded', function() {
     const section1 = document.querySelector('.section-1');
     const h1T1 = section1.querySelector('h1.t1');
     const h1T2 = section1.querySelector('h1.t2');
 
-    function handleScroll() {
-        const rect = section1.getBoundingClientRect();
-        if (rect.top >= 0 && rect.bottom <= window.innerHeight) {
-            h1T1.classList.add('animate-welcome');
-            h1T2.classList.add('animate-bemvindo');
-        } else {
-            h1T1.classList.remove('animate-welcome');
-            h1T2.classList.remove('animate-bemvindo');
-        }
-    }
+    // Executa a animação da section-1 ao carregar
+    h1T1.classList.add('animate-welcome');
+    h1T2.classList.add('animate-bemvindo');
+    h1T1.style.opacity = 1;
+    h1T2.style.opacity = 1;
 
-    window.addEventListener('scroll', handleScroll);
-    handleScroll();
+    // A função de scroll não precisa ser chamada aqui
+    // pois a animação da section-1 já foi executada no carregamento
 });
